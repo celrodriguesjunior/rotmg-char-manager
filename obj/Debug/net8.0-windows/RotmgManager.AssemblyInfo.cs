8f46fb0eb2f7ebc885679faa9b543ff669fcb368
Célio Rodrigues Junior <55858108+celrodriguesjunior@users.noreply.github.com>
2025-11-17 13:04:27 -0300

    third

--- obj/Debug/net8.0-windows/RotmgManager.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RotmgManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RotmgManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670cb2541eb9554ca55fb0cdbc2aa479e9c895bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677e61659b3475e74f7b26c62cfc7af9585b50d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RotmgManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RotmgManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
