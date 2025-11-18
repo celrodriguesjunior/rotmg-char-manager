3d131b9ec6ee71334b2b1cd504182c68f78dc887
Célio Junior <rodriguesjuniorcelio@gmail.com>
2025-11-17 23:05:30 -0300

    alterado status máximo de cada personagem

--- Persistence/GameDataRepository.cs
+++ Persistence/GameDataRepository.cs
@@ -101,14 +101,40 @@ public class GameDataRepository
     {
         var maxStats = new Dictionary<StatType, int>
         {
-            {StatType.Life, className == "Knight" ? 770 : 670},
-            {StatType.Mana, className is "Wizard" or "Necromancer" ? 385 : 252},
-            {StatType.Attack, className is "Wizard" or "Knight" ? 75 : 50},
-            {StatType.Defense, className == "Knight" ? 40 : 25},
-            {StatType.Speed, className == "Knight" ? 50 : 60},
-            {StatType.Dexterity, className is "Wizard" or "Necromancer" ? 75 : 50},
-            {StatType.Vitality, className == "Knight" ? 75 : 40},
-            {StatType.Wisdom, className == "Priest" ? 75 : 60}
+            {StatType.Life, new List<string>(){"Knight", "Warrior", "Paladin", "Ninja", "Samurai", "Kensei" }.Contains(className)  ? 800 : 
+                            new List<string>(){"Rogue", "Archer", "Assassin", "Huntress", "Trickster", "Bard" }.Contains(className) ? 750 : 
+                            700 },
+            {StatType.Mana, new List<string>(){"Wizard", "Priest", "Necromancer", "Mystic", "Sorcerer", "Bard", "Summoner" }.Contains(className)  ? 400 : 
+                            new List<string>(){"Assassin", "Huntress" }.Contains(className) ? 350 : 
+                            300},
+            {StatType.Attack, className is "Archer" or "Warrior" or "Necromancer" or "Samurai" ? 75 : 
+                              className is "Sorcerer" or "Ninja" ? 70 : 
+                              className is "Priest" or "Assassin" or "Huntress" or "Mystic" or "Trickster" or "Kensei" ? 65 : 
+                              className is "Wizard" or "Summoner" ? 60 : 
+                              className is "Rogue" or "Paladin" or "Bard" ? 55 : 
+                              50},
+            {StatType.Defense, className is "Knight" ? 40 :
+                               className is "Paladin" or "Samurai" ? 30 :
+                               25},
+            {StatType.Speed, className is "Trickster" ? 75 :
+                             className is "Rogue" or "Assassin" ? 65 :
+                             className is "Mystic" or "Sorcerer" or "Ninja" or "Summoner" or "Kensei" ? 60 :
+                             className is "Archer" or "Priest" or "Paladin" or "Samurai" or "Bard" ? 55 :
+                             50},
+            {StatType.Dexterity, className is "Wizard" or "Rogue" or "Assassin" or "Trickster" or "Summoner" ? 75 :
+                                 className is "Ninja" or "Bard" ? 70 :
+                                 className is "Mystic" or "Kensei" ? 65 :
+                                 className is "Priest" or "Assassin" or "Huntress" or "Sorcerer" ? 60 :
+                                 className is "Paladin" or "Samurai" ? 55 :
+                                 50},
+            {StatType.Vitality, className is "Warrior" or "Knight" or "Sorcerer" ? 75 :
+                                className is "Paladin" or "Ninja" or "Samurai" or "Kensei" ? 60 :
+                                className is "Bard" ? 45 :
+                                40},
+            {StatType.Wisdom, className is "Priest" or "Paladin" or "Necromancer" or "Mystic" or "Bard" or "Summoner" ? 75 :
+                              className is "Ninja" ? 70 :
+                              className is "Wizard" or "Assassin" or "Huntress" or "Trickster" or "Sorcerer" or "Samurai" ? 60 :
+                              50}
         };
 
         var gainPerPotion = new Dictionary<StatType, int>
